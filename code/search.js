cb530818f78e5dd8a7eff2954ff188098d7b28c2
PandaDap2006 <70108603+PandaDap2006@users.noreply.github.com>
2022-03-02 09:35:30 +0100

    dsgfs

--- code/search.js
+++ code/search.js
@@ -37,7 +37,6 @@ gapi.load("client:auth2", function() {
 });
 
 function createVideoList(response) {
-    //var json = JSON.parse(response)
-    var json = JSON.stringify(response)
-    console.log(json)
+    var json = JSON.parse(JSON.stringify(response))
+    console.log(json.result.items.length)
 }
